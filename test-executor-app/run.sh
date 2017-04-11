1175c59194fa94cfdfe1bccf2ed20f2a64a1f6fd
Somogyi, Benjamin <Benjamin.Somogyi@pearson.com>
2017-04-11 12:52:59 -0600

    Docker updates for testexcutor

--- test-executor-app/run.sh
+++ test-executor-app/run.sh
@@ -3,10 +3,6 @@
 export KUBE_TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
 export NAMESPACE=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
 
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" > /dev/null 2>&1
-unzip awscli-bundle.zip > /dev/null 2>&1
-./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws > /dev/null 2>&1
-
 stackips=`aws ec2 describe-instances --region=${REGION} --filters "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=tag:Stack,Values=${STACK_ID}" "Name=instance-state-code,Values=16" | jq '.Reservations[].Instances[].PrivateIpAddress' | sed -e 's/\"//g'`
 bastion=`aws ec2 describe-instances --region=${REGION} --filters "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=tag:Name,Values=bastion.${ENVIRONMENT}.kube" "Name=instance-state-code,Values=16" | jq '.Reservations[].Instances[].PrivateIpAddress' | sed -e 's/\"//g'`
 nfs=`aws ec2 describe-instances --region=${REGION} --filters "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=tag:Name,Values=nfs.${ENVIRONMENT}.kube" "Name=instance-state-code,Values=16" | jq '.Reservations[].Instances[].PrivateIpAddress' | sed -e 's/\"//g'`
@@ -19,11 +15,11 @@ chmod 600 ~/.ssh/*
 #Produce keyvalue pairs of hostnames to private ips. Would have liked to use kubectl
 #here, but servicaaccounts only allow API access to namespace this test app is
 #deployed in o. Will need to refactor this if we move away or expand outside AWS
-echo "hosts:" >> /var/hosts.yaml
+echo "hosts:" >> /opt/testexecutor/hosts.yaml
 for ip in $IPS;do
   hostname=`aws ec2 describe-instances --region=${REGION} --filters "Name=tag:Environment,Values=${ENVIRONMENT}" "Name=private-ip-address,Values=${ip}" | jq '.Reservations[].Instances[] | .Tags[] | select(.Key=="Name") | .Value' | sed -e 's/\"//g'`
-  echo "  - "name": "$hostname>> /var/hosts.yaml
-  echo "    "value": "$ip >> /var/hosts.yaml
+  echo "  - "name": "$hostname>> /opt/testexecutor/hosts.yaml
+  echo "    "value": "$ip >> /opt/testexecutor/hosts.yaml
   ssh-keyscan $ip >> ~/.ssh/known_hosts > /dev/null 2>&1
 done
 
@@ -32,6 +28,4 @@ kubectl config set-credentials ${ENVIRONMENT}-admin --client-key=/etc/secret-vol
 kubectl config set-context ${ENVIRONMENT} --cluster=${ENVIRONMENT} --user=${ENVIRONMENT}-admin
 kubectl config use-context ${ENVIRONMENT}
 
-pip install jwt
-
-python -u /var/testRunner.py "/var/hosts.yaml" $@
+python -u /opt/testexecutor/testRunner.py "/var/hosts.yaml" $@
