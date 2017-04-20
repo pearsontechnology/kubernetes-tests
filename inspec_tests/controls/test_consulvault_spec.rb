a26d9a7ac94eb30c63783c5ecc28c6af2a27c77a
Martin Devlin <martin.devlin@pearson.com>
2017-04-20 19:36:49 +0100

    Systemd for consul vault

--- inspec_tests/controls/test_consulvault_spec.rb
+++ inspec_tests/controls/test_consulvault_spec.rb
@@ -48,10 +48,6 @@ describe service('udev') do
   it { should be_enabled }
   it { should be_running }
 end
-describe service('consul') do
-  it { should be_enabled }
-  it { should be_running }
-end
 describe command('systemctl is-enabled consul') do
   its('exit_status') { should eq 0 }
 end
