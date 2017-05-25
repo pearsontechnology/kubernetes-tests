51cdcfe96bd4a1f04e8dbd50037fb90d9418c255
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-25 13:31:38 +0100

    outbound Internet test for NAT gateways

---
+++ python_tests/test_outbound_internet.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import boto3
+import os
+import yaml
+from subprocess import Popen, PIPE
+
+def run_script(command):
+    global failuresReceived
+    process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    errorCode = process.returncode
+    return stdout,stderr,errorCode
+
+def test_outbound_internet_connectivity_from_minions():
+    hostYaml="/var/hosts.yaml"
+    with open(hostYaml, 'r') as ymlfile1:  # hosts to test
+        contents = yaml.load(ymlfile1)
+        for host in contents['hosts']:
+            if ("minion" in host['name']):
+                # Verify outbound Internet connectivity from minion hosts by ping to 8.8.8.8 (Google DNS)
+                command="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 4 8.8.8.8 | grep -o '4 packets received''".format(host['value'])
+                process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                stdout, stderr = process.communicate()
+                errorCode = process.returncode
+                assert errorCode != 0   #If Error Code is non-zere, then a minion does not have outbound connectivity to the Internet
