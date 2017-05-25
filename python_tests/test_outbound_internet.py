fc921d22d26c89c06bfb2e2c21fb96f0a706ea60
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-25 16:59:32 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -4,12 +4,17 @@ import os
 import yaml
 from subprocess import Popen, PIPE
 
-def run_script(command):
+def run_script(command, output):
     global failuresReceived
     process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     errorCode = process.returncode
-    return stdout,stderr,errorCode
+    if(errorCode != 0):
+        failuresReceived = True
+    if(output):
+        #print "Received Error Code: {0}".format(errorCode)
+        print "{0}".format(stdout)
+        print "{0}".format(stderr)
 
 def test_outbound_internet_connectivity_from_minions():
     hostYaml="/opt/testexecutor/hosts.yaml"
@@ -18,7 +23,8 @@ def test_outbound_internet_connectivity_from_minions():
         for host in contents['hosts']:
             if ("minion" in host['name']):
                 # Verify outbound Internet connectivity from minion hosts by ping to 8.8.8.8 (Google DNS)
-                cmd="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 1 8.8.8.8 | grep -o '0 received''".format(host['value'])
+                command="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 1 8.8.8.8 | grep -o '0 received''".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-                stdout, stderr, errorCode=run_script(cmd)
+                stdout, stderr = process.communicate()
+                errorCode = process.returncode
                 assert errorCode != 0   #If Error Code is non-zere, then a minion does not have outbound connectivity to the Internet
