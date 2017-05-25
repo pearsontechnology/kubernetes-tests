fcc162bf523b0abf63d97740ee6ebed67323b8c3
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-25 16:50:56 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -18,7 +18,7 @@ def test_outbound_internet_connectivity_from_minions():
         for host in contents['hosts']:
             if ("minion" in host['name']):
                 # Verify outbound Internet connectivity from minion hosts by ping to 8.8.8.8 (Google DNS)
-                command="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 1 8.8.8.8 | grep -o '0 received''".format(host['value'])
+                cmd="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 1 8.8.8.8 | grep -o '0 received''".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                 stdout, stderr, errorCode=run_script(cmd)
                 assert errorCode != 0   #If Error Code is non-zere, then a minion does not have outbound connectivity to the Internet
