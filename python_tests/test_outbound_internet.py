7257dfd2c8992087b0fa9a0c3a4148929f5678c7
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 22:42:17 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import sys
 import boto3
 import os
 from subprocess import Popen, PIPE
@@ -10,12 +9,12 @@ def test_outbound_internet_connectivity_from_minions():
     with open(hostYaml, 'r') as ymlfile1:  # hosts to test
         contents = yaml.load(ymlfile1)
         for host in contents['hosts']:
-            if ("master" in host['name']):
-                command="ssh -i ~/.ssh/bitesize.key centos@{0} 'dig google.com | grep -o 'status: NOERROR''".format(host['value'])
+            if ("minion" in host['name']):
+                command="ssh -i ~/.ssh/bitesize.key -o StrictHostKeyChecking=no centos@{0} 'curl -L -I www.google.com | grep -o '200 OK' | cut -c1-3'".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                 stdout, stderr = process.communicate()
-                errorCode = process.returncode
-                assert errorCode == 0   #If Error Cod
+                response = process.stdout.read()
+                assert response == 200   #If Error Cod
 
 #if ("minion" in host['name']):
 #    command="ssh -i ~/.ssh/bitesize.key -o StrictHostKeyChecking=no centos@{0} 'curl -L -I www.google.com | grep -o '200 OK' | cut -c1-3'".format(host['value'])
