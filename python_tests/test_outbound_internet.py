5016272e591e7b506a6344d8580ab64ec5d776ee
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 14:52:54 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -16,7 +16,8 @@ def test_outbound_internet_connectivity_from_minions():
         contents = yaml.load(ymlfile1)
         for host in contents['hosts']:
             if ("minion" in host['name']):
-                command="ssh -i ~/.ssh/bitesize.key root@{0} 'ping -c 1 8.8.8.8 | grep -o '0 received''".format(host['value'])
+                returncode =
+                command="ssh -i ~/.ssh/bitesize.key root@{0} 'curl -L -I www.google.com | grep -o '200 OK''".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                 stdout,stderr,errorCode=run_script(command)
-                assert errorCode != 0   #If Error Code is non-zero, a reply has not been received from ping to Google DNS
+                assert errorCode == 0   #If Error Code is zero, www.google.com returns 200 OK
