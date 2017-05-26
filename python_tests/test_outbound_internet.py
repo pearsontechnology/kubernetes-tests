67605ee68307bc93e45a0a0c2dc09978bc4f6e98
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 12:24:52 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -3,18 +3,12 @@ import os
 from subprocess import Popen, PIPE
 import yaml
 
-
-def run_script(command, output):
+def run_script(command):
     global failuresReceived
     process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     errorCode = process.returncode
-    if(errorCode != 0):
-        failuresReceived = True
-    if(output):
-        #print "Received Error Code: {0}".format(errorCode)
-        print "{0}".format(stdout)
-        print "{0}".format(stderr)
+    return stdout,stderr,errorCode
 
 def test_outbound_internet_connectivity_from_minions():
     hostYaml="/opt/testexecutor/hosts.yaml"
@@ -24,6 +18,5 @@ def test_outbound_internet_connectivity_from_minions():
             if ("minion" in host['name']):
                 command="ssh -i ~/.ssh/bitesize.key root@{0} 'ping -c 1 8.8.8.8 | grep -o '0 received''".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-                stdout, stderr = process.communicate()
-                errorCode = process.returncode
-                assert errorCode != 0   #If Error Code is zero, a reply has not been received from ping to Google DNS
+                stdout,stderr,errorCode=run_script(cmd)
+                assert errorCode != 0   #If Error Code is non-zero, a reply has not been received from ping to Google DNS
