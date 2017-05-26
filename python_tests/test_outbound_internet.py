071afc73b7efab2a65eddc27b6947ed7dab52c7a
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 10:35:38 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python
 import boto3
 import os
-from subprocess import Popen, PIPE
 import yaml
+from subprocess import Popen, PIPE
 
-
-def run_script(command, output):
+def run_script(command):
     global failuresReceived
     process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     errorCode = process.returncode
-    if(errorCode != 0):
-        failuresReceived = True
-    if(output):
-        #print "Received Error Code: {0}".format(errorCode)
-        print "{0}".format(stdout)
-        print "{0}".format(stderr)
+    return stdout,stderr,errorCode
 
 def test_outbound_internet_connectivity_from_minions():
     hostYaml="/opt/testexecutor/hosts.yaml"
@@ -23,8 +17,8 @@ def test_outbound_internet_connectivity_from_minions():
         contents = yaml.load(ymlfile1)
         for host in contents['hosts']:
             if ("minion" in host['name']):
-                command="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 4 8.8.8.8 | grep -o '4 packets received''".format(host['value'])
-                process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-                stdout, stderr = process.communicate()
-                errorCode = process.returncode
-                assert errorCode == 0   #If Error Code is zero, then the ping to Google DNS has received 4 return packets
+                # Verify the minions have outbound connectivity to the Internet by pinging Google DNS
+                pingcount=host['value']
+                cmd="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 4 8.8.8.8 | grep -o '[0-9] received' | cut -c1'".format(host['value'])
+                stdout,stderr,errorCode=run_script(cmd)
+                assert pingcount = 4 #If Ping Count is 4, then the ping to Google DNS has received 4 return packets
