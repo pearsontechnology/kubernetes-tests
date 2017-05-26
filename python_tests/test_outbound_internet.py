0491f2e49831118c8102f89feb0adb4c441f36de
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 18:48:36 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -4,25 +4,13 @@ import os
 from subprocess import Popen, PIPE
 import yaml
 
-def run_script(command, output):
-    global failuresReceived
-    process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    errorCode = process.returncode
-    if(errorCode != 0):
-        failuresReceived = True
-    if(output):
-        #print "Received Error Code: {0}".format(errorCode)
-        print "{0}".format(stdout)
-        print "{0}".format(stderr)
-
 def test_outbound_internet_connectivity_from_minions():
     hostYaml="/opt/testexecutor/hosts.yaml"
     with open(hostYaml, 'r') as ymlfile1:  # hosts to test
         contents = yaml.load(ymlfile1)
         for host in contents['hosts']:
-            if ("minion" in host['name']):
-                command="ssh -i ~/.ssh/bitesize.key root@{0} 'curl -L -I www.google.com | grep -o '200 OK''".format(host['value'])
+            if ("master" in host['name']):
+                command="ssh -i ~/.ssh/bitesize.key StrictHostKeyChecking=no centos@{0} 'curl -L -I www.google.com | grep -o '200 OK''".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                 stdout, stderr = process.communicate()
                 errorCode = process.returncode
