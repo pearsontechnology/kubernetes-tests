f131073f19858aa4f09b96a02d93d6314a32d9b5
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 20:56:51 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
+import sys
 import boto3
 import os
 from subprocess import Popen, PIPE
 import yaml
 
 def test_outbound_internet_connectivity_from_minions():
-    hostYaml="/opt/testexecutor/hosts.yaml"
+    hostYaml="hosts.yaml"
     with open(hostYaml, 'r') as ymlfile1:  # hosts to test
         contents = yaml.load(ymlfile1)
         for host in contents['hosts']:
             if ("master" in host['name']):
-                command="ssh -i ~/.ssh/bitesize.key centos@{0} 'curl -L -I www.google.com | grep -o '200 OK''".format(host['value'])
+                command="ssh -i ~/.ssh/bitesize centos@{0} 'dig google.com | grep -o 'status: NOERROR''".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                 stdout, stderr = process.communicate()
                 errorCode = process.returncode
@@ -19,3 +20,4 @@ def test_outbound_internet_connectivity_from_minions():
 #if ("minion" in host['name']):
 #    command="ssh -i ~/.ssh/bitesize.key -o StrictHostKeyChecking=no centos@{0} 'curl -L -I www.google.com | grep -o '200 OK' | cut -c1-3'".format(host['value'])
 # dig google.com | grep -o 'status: NOERROR''
+#'curl -L -I www.google.com | grep -o '200 OK''
