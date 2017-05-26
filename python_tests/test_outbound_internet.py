679d4502b425b3b1c0094de5b0681d757c959b7f
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 23:07:35 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -13,9 +13,5 @@ def test_outbound_internet_connectivity_from_minions():
                 command="ssh -i ~/.ssh/bitesize.key -o StrictHostKeyChecking=no centos@{0} 'curl -L -I www.google.com | grep -o '200 OK' | cut -c1-3'".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                 response = process.stdout.read()
-                assert response == 200   #If Error Cod
-
-#if ("minion" in host['name']):
-#    command="ssh -i ~/.ssh/bitesize.key -o StrictHostKeyChecking=no centos@{0} 'curl -L -I www.google.com | grep -o '200 OK' | cut -c1-3'".format(host['value'])
-# dig google.com | grep -o 'status: NOERROR''
-#'curl -L -I www.google.com | grep -o '200 OK''
+                responseinteger = int(response)
+                assert responseinteger == 200   #if responseinteger is 200, a HTTP 200 response was obtained from Google
