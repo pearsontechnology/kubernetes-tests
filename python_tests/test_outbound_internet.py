f231aa83c67cdbc02562704f65283d20e8adee03
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 10:51:39 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -21,4 +21,4 @@ def test_outbound_internet_connectivity_from_minions():
                 pingcount=host['value']
                 cmd="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 4 8.8.8.8 | grep -o '[0-9] received' | cut -c1'".format(host['value'])
                 stdout,stderr,errorCode=run_script(cmd)
-                assert pingcount = 4 #If Ping Count is 4, then the ping to Google DNS has received 4 return packets
+                assert pingcount == 4 #If Ping Count is 4, then the ping to Google DNS has received 4 return packets
