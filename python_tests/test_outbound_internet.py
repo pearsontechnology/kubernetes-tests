f280c54d22aca5e289b999d1241e15642287ea1a
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 17:18:17 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -3,12 +3,17 @@ import os
 import yaml
 from subprocess import Popen, PIPE
 
-def run_script(command):
+def run_script(command, output):
     global failuresReceived
     process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     errorCode = process.returncode
-    return stdout,stderr,errorCode
+    if(errorCode != 0):
+        failuresReceived = True
+    if(output):
+        #print "Received Error Code: {0}".format(errorCode)
+        print "{0}".format(stdout)
+        print "{0}".format(stderr)
 
 def test_outbound_internet_connectivity_from_minions():
     hostYaml="/opt/testexecutor/hosts.yaml"
@@ -16,7 +21,8 @@ def test_outbound_internet_connectivity_from_minions():
         contents = yaml.load(ymlfile1)
         for host in contents['hosts']:
             if ("minion" in host['name']):
-                command="ssh -i ~/.ssh/bitesize.key root@{0} 'curl -L -I www.google.com | grep -o '200 OK' | cut -c1-3'".format(host['value'])
+                command="ssh -i ~/.ssh/bitesize.key root@{0} 'curl -L -I www.google.com | grep -o '200 OK''".format(host['value'])
                 process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-                stdout,stderr,errorCode=run_script(command)
-                assert errorCode == 200   #If returncode = 200, www.google.com returns 200 OK
+                stdout,stderr = process.communicate()
+                errorCode = process.returncode
+                assert errorCode == 0   #If the Error Code is zero, Google returned a 200 OK 
