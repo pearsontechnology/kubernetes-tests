7b498364abff8fc0777729988e2cdb85d8c8028f
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-05-26 11:36:32 +0100

    updated test

--- python_tests/test_outbound_internet.py
+++ python_tests/test_outbound_internet.py
@@ -1,24 +1,24 @@
-#!/usr/bin/python
 import boto3
 import os
-import yaml
 from subprocess import Popen, PIPE
+import yaml
+
 
 def run_script(command, output):
     global failuresReceived
     process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     errorCode = process.returncode
-    return stdout,stderr,errorCode
+    if(errorCode != 0):
+        failuresReceived = True
+    if(output):
+        #print "Received Error Code: {0}".format(errorCode)
+        print "{0}".format(stdout)
+        print "{0}".format(stderr)
 
-def test_outbound_internet_connectivity_from_minions():
-    hostYaml="/opt/testexecutor/hosts.yaml"
-    with open(hostYaml, 'r') as ymlfile1:  # hosts to test
-        contents = yaml.load(ymlfile1)
-        for host in contents['hosts']:
-            if ("minion" in host['name']):
-                # Verify the minions have outbound connectivity to the Internet by pinging Google DNS
-                pingcount=host['value']
-                cmd="ssh -i ~/.ssh/bitesize.key centos@{0} 'ping -c 4 8.8.8.8 | grep -o '[0-9] received' | cut -c1'".format(host['value'])
-                stdout,stderr,errorCode=run_script(cmd)
-                assert pingcount == 4 #If Ping Count is 4, then the ping to Google DNS has received 4 return packets
+def test_outbound_internet_connectivity_from_minions(domain,ip):
+    command = "ssh -i ~/.ssh/bitesize.key centos@{0} 'TIMEFORMAT=%R;time dig $domain'".format(ip)
+    process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    errorCode = process.returncode
+    return stderr
