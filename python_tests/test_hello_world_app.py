91c36a0d408f60ddd2ee99ff9a62548916e540fc
Benjamin Somogyi <benjamin.somogyi@pearson.com>
2017-06-02 11:05:34 -0600

    Add new test to check hello-world test app

---
+++ python_tests/test_hello_world_app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import boto3
+import os
+from subprocess import Popen, PIPE
+import yaml
+
+
+def run_script(command, output):
+    global failuresReceived
+    process = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    errorCode = process.returncode
+    if(errorCode != 0):
+        failuresReceived = True
+    if(output):
+        #print "Received Error Code: {0}".format(errorCode)
+        print "{0}".format(stdout)
+        print "{0}".format(stderr)
+    return stdout, stderr, errorCode
+
+def test_hello_world_app_responds_through_bitesize_front_end():
+    cmd = "curl -s -m 5 -o /dev/null -w \"%{http_code}\" -X POST -H 'Host: front.nodejs-hello-world-app.prsn-dev.io' -H \"Content-Type: application/json\" -d '{\"data\": \"blah\", \"username\": \"admin\", \"password\": \"password\"}' http://front.nodejs-hello-world-app.prsn-dev.io | grep 200"
+    stdout = run_script(cmd, False)
+    assert stdout = "200"
