89e3d3cf9c7ed9e7b68fabe3ae3097b08582218d
Benjamin Somogyi <benjamin.somogyi@pearson.com>
2017-06-09 10:58:21 -0600

    test updates

--- python_tests/test_consul.py
+++ python_tests/test_consul.py
@@ -8,6 +8,7 @@ def test_consul_read_and_write():
     hostYaml="/opt/testexecutor/hosts.yaml"
     env = os.environ["ENVIRONMENT"]
     dom = os.environ["DOMAIN"]
+    token = os.environ["CONSUL_MASTER_TOKEN"]
     with open(hostYaml, 'r') as ymlfile1:  # hosts to test
         contents = yaml.load(ymlfile1)
         hostYaml="/opt/testexecutor/hosts.yaml"
@@ -17,22 +18,22 @@ def test_consul_read_and_write():
                 if ("master" in host['name']):
                     ip=host['value']
 
-                    curlwrite = "curl -ks -X PUT -d 'test_value' https://consul.bitesize.${ENVIRONMENT}.${DOMAIN}/v1/kv/test/KEY1?token=${CONSUL_MASTER_TOKEN}"
+                    curlwrite = "curl -ks -X PUT -d 'test_value' https://consul.bitesize.${env}.${dom}/v1/kv/test/KEY1?token=${token}"
                     cmd="ssh -i ~/.ssh/bitesize.key -o StrictHostKeyChecking=no centos@{0} '{1}'".format(ip,curlwrite)
                     process = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                     stdout, stderr = process.communicate()
                     errorCode = process.returncode
-                    #print "Stdout:{0}".format(stdout)
-                    #print "Stderr:{0}".format(stderr)
-                    #print "errorCode:{0}".format(errorCode)
+                    print "Stdout:{0}".format(stdout)
+                    print "Stderr:{0}".format(stderr)
+                    print "errorCode:{0}".format(errorCode)
                     assert errorCode == 200
 
-                    curlread = "curl -ks https://consul.bitesize.${ENVIRONMENT}.${DOMAIN}/v1/kv/test/KEY1?token=${CONSUL_MASTER_TOKEN} | jq '.[0].Value' | sed -e s/\"//g | base64 -d"
+                    curlread = "curl -ks https://consul.bitesize.${env}.${dom}/v1/kv/test/KEY1?token=${token} | jq '.[0].Value' | sed -e s/\"//g | base64 -d"
                     cmd="ssh -i ~/.ssh/bitesize.key -o StrictHostKeyChecking=no centos@{0} '{1}'".format(ip,curlread)
                     process = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
                     stdout, stderr = process.communicate()
                     errorCode = process.returncode
-                    #print "Stdout:{0}".format(stdout)
-                    #print "Stderr:{0}".format(stderr)
-                    #print "errorCode:{0}".format(errorCode)
+                    print "Stdout:{0}".format(stdout)
+                    print "Stderr:{0}".format(stderr)
+                    print "errorCode:{0}".format(errorCode)
                     assert stdout == "test_value"
