fecf780a59119178e4f6a95c664c1eb58c100a22
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-06-12 11:36:10 +0100

    test for audit and syslog cloudwatch log groups

---
+++ python_tests/test_cloudwatch_os_logs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import boto3
+import os
+
+##Verify cloudwatch log groups exist for audit and syslog
+def test_cloudwatch_os_log_groups_exist():
+    logs = boto3.client('logs', region_name=os.environ["REGION"])
+    env = os.environ["ENVIRONMENT"]
+    log_group_count = 0
+    log_groups = logs.describe_log_groups()
+    for log in log_groups ['logGroups']:
+        if (env in log['logGroupName']) and log['logGroupName'].startswith("/bitesize"):
+            if log['logGroupName'].endswith("/audit"):
+                log_group_count += 1
+            if log['logGroupName'].endswith("/syslog"):
+                log_group_count += 1
+    try:
+        assert log_group_count ==  4  #Should be four log groups
+        print 'correct number of Cloudwatch audit and syslog log groups'
+    except:
+        print 'incorrect number of Cloudwatch audit and syslog log groups'
+
+test_cloudwatch_os_log_groups_exist()
