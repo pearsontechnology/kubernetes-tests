24676276b8129aaeeb15ef7afa5565a0115c64d5
Fernando, Ishara <ishara.fernando@pearson.com>
2017-06-13 12:28:31 +0530

    Changed travis to ssh on the master as 'centos' user instead of 'root' user

--- test-executor-app/testRunner.py
+++ test-executor-app/testRunner.py
@@ -74,7 +74,7 @@ def executeTest(hostname, ip, testFiles=None):
                             if (resourceName == "nfs" or resourceName == "bastion" or resourceName == "stackstorm"):
                                 cmd = "inspec exec {0}{1} -t \"ssh://root@{2}\" --key-files=\"~/.ssh/bitesize.key\"".format(inspecDir, test, ip)
                             elif (resourceName == "master"):
-                                cmd = "inspec exec {0}{1} -t \"ssh://root@{2}\" --key-files=\"~/.ssh/bitesize.key\" --sudo".format(inspecDir, test, ip)
+                                cmd = "inspec exec {0}{1} -t \"ssh://centos@{2}\" --key-files=\"~/.ssh/bitesize.key\" --sudo".format(inspecDir, test, ip)
                             else:
                                 cmd = "inspec exec {0}{1} -t \"ssh://centos@{2}\" --key-files=\"~/.ssh/bitesize.key\"".format(inspecDir, test, ip)
                             print("Testing on HostName=%s with IP=%s" % (hostname, ip))
