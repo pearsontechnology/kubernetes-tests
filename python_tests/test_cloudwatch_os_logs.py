cb3bb5f002410792d015163aa37c7320985a4f0a
Paul Schwarzenberger <paul.schwarzenberger@pearson.com>
2017-06-13 07:34:56 +0100

    log stream tests added

--- python_tests/test_cloudwatch_os_logs.py
+++ python_tests/test_cloudwatch_os_logs.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 import boto3
+import sys
 import os
+import time
+import json
+import argparse
+
+from botocore.exceptions import ClientError
+from datetime import datetime
 
 ##Verify cloudwatch log groups exist for audit and syslog
 def test_cloudwatch_os_log_groups_exist():
@@ -21,17 +28,25 @@ def test_cloudwatch_audit_log_streams():
     logs = boto3.client('logs', region_name=os.environ["REGION"])
     env = os.environ["ENVIRONMENT"]
     log_groups = logs.describe_log_groups()
-    for log in log_groups ['logGroups']:
-        if (env in log['logGroupName']) and log['logGroupName'].startswith("/bitesize") and log['logGroupName'].endswith("/audit"):
-            log_streams = logs.describe_log_streams(logGroupName = log['logGroupName'])
-        assert len(log_streams) >= 3   #should be at least 6 Cloudwatch audit log streams
+    audit_log_stream_count = 0
+    for log_group in log_groups ['logGroups']:
+        if (env in log_group['logGroupName']) and log_group['logGroupName'].startswith("/bitesize") and log_group['logGroupName'].endswith("/audit"):
+            log_stream_data = logs.describe_log_streams(logGroupName = log_group['logGroupName'])
+            for log_stream in log_stream_data['logStreams']:
+                if (log_stream['logStreamName'].startswith("10.") or log_stream['logStreamName'].startswith("172.")):
+                    audit_log_stream_count +=1
+    assert audit_log_stream_count >= 3   #should be at least 3 Cloudwatch audit log streams
 
-##Verify audit log streams written to Cloudwatch log groups
+##Verify syslog log streams written to Cloudwatch log groups
 def test_cloudwatch_syslog_log_streams():
     logs = boto3.client('logs', region_name=os.environ["REGION"])
     env = os.environ["ENVIRONMENT"]
     log_groups = logs.describe_log_groups()
-    for log in log_groups ['logGroups']:
-        if (env in log['logGroupName']) and log['logGroupName'].startswith("/bitesize") and log['logGroupName'].endswith("/syslog"):
-            log_streams = logs.describe_log_streams(logGroupName = log['logGroupName'])
-        assert len(log_streams) >= 3   #should be at least 6 Cloudwatch audit log streams   #should be at least 6 Cloudwatch syslog log streams
+    syslog_log_stream_count = 0
+    for log_group in log_groups ['logGroups']:
+        if (env in log_group['logGroupName']) and log_group['logGroupName'].startswith("/bitesize") and log_group['logGroupName'].endswith("/syslog"):
+            log_stream_data = logs.describe_log_streams(logGroupName = log_group['logGroupName'])
+            for log_stream in log_stream_data['logStreams']:
+                if (log_stream['logStreamName'].startswith("10.") or log_stream['logStreamName'].startswith("172.")):
+                    syslog_log_stream_count +=1
+    assert syslog_log_stream_count >= 3   #   #should be at least 3 Cloudwatch syslog log streams   #should be at least 6 Cloudwatch syslog log streams
