188363651e75599d00bdff560dc1ee210331243a
Martin Devlin <martin.devlin@pearson.com>
2017-09-07 11:43:29 +0100

    Add key back in

--- python_tests/test_vault_tokens.py
+++ python_tests/test_vault_tokens.py
@@ -31,21 +31,21 @@ def test_manually_verify_tokens():
     global writeTokenTtl
     global readToken
     global writeToken
-    cmd="ssh -q centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-read --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-read --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     readToken=stdout.rstrip()
     uuidPattern.match(readToken)
-    cmd="ssh -q centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-write --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-write --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     writeToken=stdout.rstrip()
     uuidPattern.match(writeToken)
-    cmd="ssh -q centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,readToken)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,readToken)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     readTokenTtl=stdout.rstrip()
-    cmd="ssh -q centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,writeToken)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,writeToken)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     writeTokenTtl=stdout.rstrip()
@@ -54,39 +54,40 @@ def test_check_ns_tokens():
     global readTokenTtlNow
     global writeTokenTtlNow
     time.sleep(2)
-    cmd="ssh -q centos@{0} \'sudo su - -c \"/usr/local/bin/renew_vault_tokens.sh check tokens-test\"\' | grep ^TTL\: | awk \'{{print $2}}\' | head -1".format(master)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo su - -c \"/usr/local/bin/renew_vault_tokens.sh check tokens-test\"\' | grep ^TTL\: | awk \'{{print $2}}\' | head -1".format(master)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     readTokenTtlNow=stdout.rstrip()
-    cmd="ssh -q centos@{0} \'sudo su - -c \"/usr/local/bin/renew_vault_tokens.sh check tokens-test\"\' | grep ^TTL\: | awk \'{{print $2}}\' | tail -1".format(master)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo su - -c \"/usr/local/bin/renew_vault_tokens.sh check tokens-test\"\' | grep ^TTL\: | awk \'{{print $2}}\' | tail -1".format(master)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     writeTokenTtlNow=stdout.rstrip()
-
+    assert int(readTokenTtl) > int(readTokenTtlNow)
+    assert int(writeTokenTtl) > int(writeTokenTtlNow)
     assert int(readTokenTtl) / int(readTokenTtlNow) == 1
-    assert int(readTokenTtl) / int(writeTokenTtlNow) == 1
+    assert int(writeTokenTtl) / int(writeTokenTtlNow) == 1
 
 def test_regen_tokens():
-    cmd="ssh -q centos@{0} \'sudo su - -c \"/usr/local/bin/renew_vault_tokens.sh regen tokens-test\"\'".format(master)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo su - -c \"/usr/local/bin/renew_vault_tokens.sh regen tokens-test\"\'".format(master)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
-    cmd="ssh -q centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-read --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-read --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     readTokenNew=stdout.rstrip()
     uuidPattern.match(readTokenNew)
     assert readTokenNew != readToken
-    cmd="ssh -q centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-write --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo kubectl get --no-headers secret vault-tokens-test-write --namespace=tokens-test -o json\' | jq -r \'.data[]\' | base64 -d".format(master)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     writeTokenNew=stdout.rstrip()
     uuidPattern.match(writeTokenNew)
     assert writeTokenNew != writeToken
-    cmd="ssh -q centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,readTokenNew)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,readTokenNew)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     readTokenNewTtl=stdout.rstrip()
-    cmd="ssh -q centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,writeTokenNew)
+    cmd="ssh -q -i ~/.ssh/bitesize.key centos@{0} \'sudo su - -c \"vault token-lookup {1}\"\' | grep ^ttl | awk \'{{print $NF}}\'".format(master,writeTokenNew)
     stdout,stderr,errorCode=run_script(cmd)
     assert errorCode == 0
     writeTokenNewTtl=stdout.rstrip()
