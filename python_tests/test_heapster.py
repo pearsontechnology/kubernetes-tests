1ff33ace285263d87b6f022a29752fd5158a59ae
cristian-radu <cristian.radu@pearson.com>
2017-09-25 17:21:09 +0100

    Fix test not loaded by node2

--- python_tests/test_heapster.py
+++ python_tests/test_heapster.py
@@ -2,13 +2,11 @@ import json
 import requests
 
 # Check a random container to see if heapster is collecting the CPU metric.
-def test_heapster_is_collecting_container_cpu_metric(namespace, container):
-    req = requests.get("http://heapster.kube-system.svc.cluster.local/apis/metrics/v1alpha1/namespaces/" + namespace + "/pods")
+def test_heapster_is_collecting_container_cpu_metric():
+    req = requests.get("http://heapster.kube-system.svc.cluster.local/apis/metrics/v1alpha1/namespaces/kube-system/pods")
     cpu_metric = ""
     for pod in json.loads(req.text)['items']:
         for c in pod['containers']:
-            if c['name'] ==  container:
+            if c['name'] == "kube-apiserver":
                 cpu_metric = c['usage']['cpu']
     assert(cpu_metric != "")
-
-test_heapster_is_collecting_container_cpu_metric("kube-system", "kube-apiserver")
